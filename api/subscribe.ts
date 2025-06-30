95ee5ee0b013670396cae3cfb866b152383da2c2
slarblar <jordanwindex@gmail.com>
2025-06-30 03:57:01 -0600

    Trigger redeployment: Add comment to API file

--- api/subscribe.ts
+++ api/subscribe.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-// TypeScript interfaces
+// TypeScript interfaces for MailerLite API
 interface SubscribeRequestBody {
   email: string;
 }
